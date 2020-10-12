26aa8597ab32700b821395c52381099ed74f8b25
大橋 <ohashi.wr@gmail.com>
2020-10-12 13:48:20 +0900

    スクロールアニメーションの追加

--- js/script.js
+++ js/script.js
@@ -116,3 +116,20 @@ document.addEventListener("click", e => {
     });
 
 }
+
+// 参考url http://ishi-note.com/6814.html
+// スクロールフェードイン
+window.addEventListener("DOMContentLoaded", function() {
+  window.onscroll = function (){
+    var myFade = document.getElementsByClassName("fadein");
+    for(var i = 0; i < myFade.length; i++){
+      var targetElement = myFade[i].getBoundingClientRect(); //ターゲット要素の高さ
+      var scroll = document.documentElement.scrollTop || document.body.scrollTop;  //スクロール
+      var windowHeight = window.innerHeight;  //ウィンドウの高さ
+      if (scroll > scroll + targetElement.top - windowHeight + 200){
+        myFade[i].style.opacity = "1";
+        myFade[i].style.transform = "translateY(0)";
+      }
+    }
+  }
+}, false);
